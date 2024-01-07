17c5d6769ae6d9b0bc0316e9575f8fe8745eb736
Pax Sharma <ps2340@bath.ac.uk>
2024-01-07 15:21:52 +0000

    initialised web frontend client using Next

---
+++ vidshare-web-client/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="">
+      <div className="">
+        <p className="">
+          Get started by editing&nbsp;
+          <code className="">app/page.tsx</code>
+        </p>
+      </div>
+    </main>
+  )
+}
